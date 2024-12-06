2c282290d42decf5e3695c5d06aa4162491a5c35
Frank Reininghaus <frank78ac@googlemail.com>
2024-12-06 21:13:02 +0100

    Refactoring

--- 2024/src/day04.rs
+++ 2024/src/day04.rs
@@ -12,27 +12,48 @@ fn main() {
     println!("Part 2: {}", part2(&data));
 }
 
-fn part1(lines: &str) -> usize {
-    let word_search: Vec<Vec<char>> = lines.lines()
-        .map(|line| line.chars().filter(|&c| c.is_ascii_alphabetic()).collect())
-        .collect::<Vec<_>>();
+struct WordSearch {
+    n: usize,
+    data: Vec<Vec<char>>
+}
 
-    let width = word_search[0].len();
-    let height = word_search.len();
+impl WordSearch {
+    fn new(lines: &str) -> Self {
+        let data: Vec<Vec<char>> = lines.lines()
+            .map(|line| line.chars().filter(|&c| c.is_ascii_alphabetic()).collect())
+            .collect();
+        
+        let n = data.len();
+        
+        if !data.iter().all(|row| row.len() == n) {
+            panic!("Expected a square grid of letters");
+        }
+        
+        Self { n, data }
+    }
+
+    fn line(&self, points: &Vec<(usize, usize)>) -> String {
+        points.into_iter().map(|(x, y)| self.data[*y][*x]).collect()
+    }
+}
+
+fn part1(lines: &str) -> usize {
+    let word_search = WordSearch::new(lines); 
+    let n = word_search.n;
+
+    let rows = (0..n).map(|y| (0..n).map(|x| (x, y)).collect());
+    let columns = (0..n).map(|x| (0..n).map(|y| (x, y)).collect());
     
-    let rows = (0..height).map(|y| (0..width).map(|x| (x, y)).collect());
-    let columns = (0..width).map(|x| (0..height).map(|y| (x, y)).collect());
+    let diag_down_1 = (0..n).map(|x0| (x0..n).map(|x| (x, x - x0)).collect());
+    let diag_down_2 = (1..n).map(|y0| (0..n - y0).map(|x| (x, y0 + x)).collect());
     
-    let diag_down_1 = (0..width).map(|x0| (x0..width).map(|x| (x, x - x0)).collect());
-    let diag_down_2 = (1..height).map(|y0| (0..width - y0).map(|x| (x, y0 + x)).collect());
-    
-    let diag_up_1 = (0..width).map(|x0| (x0..width).map(|x| (x, height - 1 - (x - x0))).collect());
-    let diag_up_2 = (0..height - 1).map(|y0| (0..y0 + 1).map(|x| (x, y0 - x)).collect());
+    let diag_up_1 = (0..n).map(|x0| (x0..n).map(|x| (x, n - 1 - (x - x0))).collect());
+    let diag_up_2 = (0..n - 1).map(|y0| (0..y0 + 1).map(|x| (x, y0 - x)).collect());
 
 
     let all_lines: Vec<Vec<_>> = rows.chain(columns).chain(diag_down_1).chain(diag_down_2).chain(diag_up_1).chain(diag_up_2).collect();
     
-    let all_words: Vec<String> = all_lines.iter().map(|points| points.iter().map(|(x, y)| word_search[*y][*x]).collect()).collect();
+    let all_words: Vec<String> = all_lines.iter().map(|points| word_search.line(&points)).collect();
     
     let haystack = all_words.join(" ");
     
