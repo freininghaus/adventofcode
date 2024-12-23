5bd50a1607a4ee35abe07f4209efb42ff12a7035
Frank Reininghaus <frank78ac@googlemail.com>
2024-12-23 23:43:54 +0100

    2024, day 12, part 2

--- 2024/src/day12.rs
+++ 2024/src/day12.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use itertools::Itertools;
 
 fn input() -> String {
     std::fs::read_to_string("input/day12.txt")
@@ -95,7 +96,54 @@ fn part1(data: &str) -> usize {
 }
 
 fn part2(data: &str) -> usize {
-    0
+    let regions = parse(data);
+
+    regions.into_iter()
+        .map(|(_c, plots)| {
+            let area = plots.len();
+
+            let up = (0, -1);
+            let down = (0, 1);
+            let left = (-1, 0);
+            let right = (1, 0);
+
+            let sides =  [up, down, left, right].into_iter()
+                .map(|(dx, dy)| {
+                    plots.iter()
+                        // Find plots which are part of a side in the given direction.
+                        .filter(|(x, y)| !plots.contains(&(*x + dx, *y + dy)))
+                        .map(
+                            // To simplify further processing, we swap x and y in the up/down case.
+                            // This ensures that x is the coordinate which is the same for all plots
+                            // that belong to a side, and y is the coordinate that grows in steps
+                            // of one unit between plots in the side.
+                            |(x, y)|
+                                if dx == 0 {
+                                    (*y, *x)
+                                } else {
+                                    (*x, *y)
+                                }
+                        )
+                        .sorted()
+                        // Use chunk_by to group into points with the same 'x' value
+                        .chunk_by(|(x, _y)| *x)
+                        .into_iter()
+                        .map(
+                            |(_x, plots)|
+                                plots
+                                    .map(|(_x, y)| y)
+                                    .collect::<Vec<_>>()
+                                    // Use chunk by to group into points whose y values differ by one
+                                    .chunk_by(|y1, y2| y1 + 1 == *y2)
+                                    .count()
+                        )
+                        .sum::<usize>()
+                })
+                .sum::<usize>();
+
+            area * sides
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -120,6 +168,6 @@ MMMISSJEEE";
 
     #[test]
     fn test_part2() {
-        assert_eq!(42, part2(TEST_INPUT));
+        assert_eq!(1206, part2(TEST_INPUT));
     }
 }
